451a414eb3f63cd486201bc438531512e4915e7a
Daan Posthumus <daan0308@gmail.com>
2025-08-11 16:11:47 +0000

    Update 7 files
    - /.devcontainer/.gitkeep
    - /.devcontainer/devcontainer.json
    - /.devcontainer/Dockerfile
    - /.devcontainer/arm-none-eabi-toolchain.cmake
    - /.vscode/launch.json
    - /.vscode/tasks.json
    - /.gitignore

---
+++ .devcontainer/arm-none-eabi-toolchain.cmake
@@ -0,0 +1,14 @@
+set(CMAKE_SYSTEM_NAME Generic)
+set(CMAKE_SYSTEM_PROCESSOR arm)
+
+set(CMAKE_C_COMPILER arm-none-eabi-gcc)
+set(CMAKE_CXX_COMPILER arm-none-eabi-g++)
+set(CMAKE_ASM_COMPILER arm-none-eabi-gcc)
+
+set(CMAKE_AR arm-none-eabi-ar)
+set(CMAKE_OBJCOPY arm-none-eabi-objcopy)
+set(CMAKE_OBJDUMP arm-none-eabi-objdump)
+set(CMAKE_SIZE arm-none-eabi-size)
+set(CMAKE_STRIP arm-none-eabi-strip)
+
+set(CMAKE_EXE_LINKER_FLAGS "-nostdlib")
