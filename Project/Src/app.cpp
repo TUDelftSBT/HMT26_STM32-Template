502dcf614a98ae05f01139454f1631b3a05c9ddb
MadCowSimulator <101363323+MadCowSimulator@users.noreply.github.com>
2026-02-02 22:50:30 +0100

    adding app code

---
+++ Project/Src/app.cpp
@@ -0,0 +1,16 @@
+//
+// Created by Teund on 02/02/2026.
+//
+
+#include "app.h"
+
+
+void app_init(void) {
+    // Initialization of the application
+}
+
+void app_loop(void) {
+    __asm__("nop"); // nop does nothing
+
+    // but cool code can go here
+}
