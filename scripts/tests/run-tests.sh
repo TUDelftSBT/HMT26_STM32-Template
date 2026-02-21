5e6187c3db59d17be9e36fdb777f748150c1a576
Erik van Weelderen <erikvanweelderen@hotmail.nl>
2026-02-21 17:34:00 +0000

    Compile test and build with flags

--- scripts/tests/run-tests.sh
+++ scripts/tests/run-tests.sh
@@ -3,6 +3,6 @@
 # To be ran from within the devcontainer
 mkdir -p ./build-test-on-host
 
-cmake -S tests/on-host -B ./build-test-on-host -DCMAKE_BUILD_TYPE=Debug
-cmake --build ./build-test-on-host -j
+cmake -S tests/on-host -B ./build-test-on-host -G Ninja -DCMAKE_BUILD_TYPE=Debug -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
+cmake --build ./build-test-on-host -j --parallel
 ctest --test-dir ./build-test-on-host --output-on-failure
