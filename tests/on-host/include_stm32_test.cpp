e425e2d19018c2947b0c6f8b04d6df8ea9272410
Erik van Weelderen <erikvanweelderen@hotmail.nl>
2026-03-03 16:44:25 +0000

    Add comment and template for a new test

--- tests/on-host/include_stm32_test.cpp
+++ tests/on-host/include_stm32_test.cpp
@@ -6,3 +6,11 @@ TEST(IncludeSTM32, CanIncludeFile)
 {
     EXPECT_TRUE(true);
 }
+
+// TODO: Add FFF (https://github.com/meekrosoft/fff)
+// Then make this test pass
+TEST(IncludeSTM32, CanCallFakeFunction)
+{
+    // MX_CAN2_Init();
+    // EXPECT_TRUE(true);
+}
