d6572bb02ec0edf398d6e81f7cf8b8827a741ba3
Erik van Weelderen <erikvanweelderen@hotmail.nl>
2026-03-03 16:15:55 +0000

    Add core header files to the project library, does NOT work for tests yet

--- Project/hmt-compile-project-lib.cmake
+++ Project/hmt-compile-project-lib.cmake
@@ -21,7 +21,6 @@ target_compile_definitions(HMT_PROJECT_LIB PRIVATE
     DEBUG=$<BOOL:$<CONFIG:Debug>>
 )
 
-
 # https://www.reddit.com/r/embedded/comments/1exl3r8/what_compiler_flags_do_you_guys_use_for_day_to/
 target_compile_options(HMT_PROJECT_LIB PRIVATE
     -Wall
@@ -43,3 +42,31 @@ target_compile_options(HMT_PROJECT_LIB PRIVATE
     -Winvalid-pch
     -Wodr
 )
+
+
+# Add the STM32 headers to the project library.
+# That way the compiler can know where to find the header files.
+# These are then later linked in.
+# Still it ignores the warnings on the include dirs by using SYSTEM INTERFACE
+
+# This also allows to later fake the libraries in testing by using some conditional 
+# compilation
+add_library(STM32_HEADERS INTERFACE)
+
+# These are the include dirs of the variable: MX_Include_Dirs
+# from the STM32Cube generated cmake file.
+target_include_directories(STM32_HEADERS SYSTEM INTERFACE
+    ${HMT_PROJECT_LIB_DIR}/../Core/Inc
+    ${HMT_PROJECT_LIB_DIR}/../Drivers/STM32F4xx_HAL_Driver/Inc
+    ${HMT_PROJECT_LIB_DIR}/../Drivers/STM32F4xx_HAL_Driver/Inc/Legacy
+    ${HMT_PROJECT_LIB_DIR}/../Drivers/CMSIS/Device/ST/STM32F4xx/Include
+    ${HMT_PROJECT_LIB_DIR}/../Drivers/CMSIS/Include
+)
+
+# Also copied from the STM32Cube cmake file.
+target_compile_definitions(STM32_HEADERS INTERFACE
+	USE_HAL_DRIVER 
+	STM32F446xx
+)
+
+target_link_libraries(HMT_PROJECT_LIB PUBLIC STM32_HEADERS)
