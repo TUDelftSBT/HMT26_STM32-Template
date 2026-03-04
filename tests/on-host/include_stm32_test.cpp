1e5b05772e42b37a7d444c7227b74075b19731e4
Erik van Weelderen <erikvanweelderen@hotmail.nl>
2026-03-04 08:34:50 +0000

    Working FFF for tests

--- tests/on-host/include_stm32_test.cpp
+++ tests/on-host/include_stm32_test.cpp
@@ -1,16 +1,17 @@
 #include <gtest/gtest.h>
 
-#include "can.h"
+#include "fff.h"
+DEFINE_FFF_GLOBALS;
 
 TEST(IncludeSTM32, CanIncludeFile)
 {
     EXPECT_TRUE(true);
 }
 
-// TODO: Add FFF (https://github.com/meekrosoft/fff)
-// Then make this test pass
-TEST(IncludeSTM32, CanCallFakeFunction)
+
+FAKE_VOID_FUNC(MX_CAN2_Init);
+TEST(IncludeSTM32, FakeFunction)
 {
-    // MX_CAN2_Init();
-    // EXPECT_TRUE(true);
+    MX_CAN2_Init();
+    EXPECT_EQ(MX_CAN2_Init_fake.call_count, 1);
 }
