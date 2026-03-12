4183b00b09c2a852d503b3d349a2d769bfc4adab
Erik van Weelderen <erikvanweelderen@hotmail.nl>
2026-03-12 08:58:16 +0000

    Can send over UART

--- Project/Src/app.cpp
+++ Project/Src/app.cpp
@@ -3,14 +3,16 @@
 
 void app_init(void) {
     // Initialization of the application
-    TriceInit();
-    #if !TRICE_OFF
-    SysTick->CTRL |= SysTick_CTRL_TICKINT_Msk; // enable SysTick interrupt
-    #endif
+    // TriceInit();
+    // #if !TRICE_OFF
+    // SysTick->CTRL |= SysTick_CTRL_TICKINT_Msk; // enable SysTick interrupt
+    // #endif
 }
 
 void app_loop(void) {
-    trice("Hello world!");
-    HAL_Delay(400); 
-    TriceTransfer(); // serve deferred output<<<
+    // trice("Hello world!");
+    // TriceTransfer(); // serve deferred output<<<
+
+    char c = 'A';
+    HAL_UART_Transmit(&huart2, (uint8_t*)&c, 1, HAL_MAX_DELAY);
 }
