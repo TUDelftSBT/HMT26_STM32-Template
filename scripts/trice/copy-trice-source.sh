5bf9b35e1981ed696221eb2bab082ad3af001c18
Erik van Weelderen <erikvanweelderen@hotmail.nl>
2026-03-12 15:13:34 +0000

    Ensure that trice source is installed in right place

--- scripts/trice/copy-trice-source.sh
+++ scripts/trice/copy-trice-source.sh
@@ -2,4 +2,4 @@
 
 # The argument must be ${localWorkspaceFolderBasename}
 mkdir -p /workspaces/$1/Libs/trice/trice/
-cp -r /opt/trice/src/ /workspaces/$1/Libs/trice/trice/
+cp -r /opt/trice/src/* /workspaces/$1/Libs/trice/trice/
