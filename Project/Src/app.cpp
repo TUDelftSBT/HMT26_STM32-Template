013c159e15d91efa12323e38eaee0e9e4f7ae76d
Erik van Weelderen <erikvanweelderen@hotmail.nl>
2026-03-12 11:27:23 +0000

    Initialisation print

--- Project/Src/app.cpp
+++ Project/Src/app.cpp
@@ -2,15 +2,15 @@
 
 
 void app_init(void) {
-    // Initialization of the application
-    // TriceInit();
-    // #if !TRICE_OFF
-    // SysTick->CTRL |= SysTick_CTRL_TICKINT_Msk; // enable SysTick interrupt
-    // #endif
+    TriceInit();
+    #if !TRICE_OFF
+    SysTick->CTRL |= SysTick_CTRL_TICKINT_Msk; // enable SysTick interrupt
+    #endif
+
+    trice("INFO: Initialisation complete. Trice is also working! :) \n");
 }
 
 void app_loop(void) {
-    TRice(iD(6558), "TRICE BROERTJE!!!!\n"); // with "fixed" iD(170), 32-bit stamp, and with `\n`
     TriceTransfer();
 
     HAL_GPIO_TogglePin(LED_DEBUG_2_GPIO_Port, LED_DEBUG_2_Pin);
