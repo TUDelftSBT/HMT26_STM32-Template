5d8d36f6cb826ce7f19cb0efa5d71217751a5ba6
Erik van Weelderen <erikvanweelderen@hotmail.nl>
2026-03-16 10:00:49 +0000

    Only update trice source if does not exist yet. Prevents recompilation

--- scripts/trice/copy-trice-source.sh
+++ scripts/trice/copy-trice-source.sh
@@ -2,4 +2,6 @@
 
 # The argument must be ${localWorkspaceFolderBasename}
 mkdir -p /workspaces/$1/Libs/trice/trice/
-cp -r /opt/trice/src/* /workspaces/$1/Libs/trice/trice/
+
+# Prevent recompilation by using -n flag
+cp -r -n /opt/trice/src/* /workspaces/$1/Libs/trice/trice/
